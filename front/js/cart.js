4bb4c920598c018a254832e2173041dc6bb6477b
Camille Ollivier <camilleollivier@yahoo.fr>
2022-04-11 18:13:53 +0200

    commit

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,101 +1,162 @@
 
-// localStorage.setItem ("_id", idProduct);
-// localStorage.setItem ("colors", color);
-// localStorage.setItem ("quantity", document.querySelector("#quantity").value);
 
 
-//Récupérer et afficher les données sur la page web
-let idProduct=localStorage.getItem("_id");
-let colorProduct=localStorage.getItem("colors")
-let quantityProduct=localStorage.getItem("quantity")
+//Récupérer les données de localStorage : stockées sous forme ('product', {id, couleur, quantité})
 
-let arrayCart =[idProduct,colorProduct,quantityProduct];
-//console.log(arrayCart)
+//déclarer la variable qui contient les 3 valeurs
+let inCart = JSON.parse(localStorage.getItem('cart'));
+console.log(inCart);
 
-//const productIntoCart = arrayCart.find(element=>element._id===_id)
+//récuperer les 3 valeurs du canapé au panier
+for (let product of inCart) {
+  let idProduct = product._id;
+  let quantityProduct = product.quantity;
+  let colorProduct = product.colors;
+  console.log(product);
+  fetch('http://localhost:3000/api/products/' + idProduct)
+    .then((response) => response.json())
+    .then((promise) => {
+      let productDescription = promise;
 
-//récupérer tous les produits, afin de...
+      let productArticle = document.createElement("article")
+      document.getElementById('cart__items').appendChild(productArticle)
+      productArticle.classList.add("cart__item")
+  
+      let productDiv1 = document.createElement("div")
+      productArticle.appendChild(productDiv1)
+      productDiv1.classList.add("cart__item__img")
+  
+      let imgProduct = document.createElement("img")
+      productDiv1.appendChild(imgProduct)
+      imgProduct.setAttribute("src", productDescription.imageUrl);
+      imgProduct.setAttribute("alt", productDescription.altTxt);
+  
+      let productDiv2 = document.createElement("div")
+      productArticle.appendChild(productDiv2)
+      productDiv2.classList.add("cart__item__content")
+  
+      let productDiv3 = document.createElement("div")
+      productDiv2.appendChild(productDiv3)
+      productDiv3.classList.add("cart__item__content__description")
+  
+      let productName = document.createElement("h2")
+      productDiv3.appendChild(productName)
+      productName.innerHTML = productDescription.name
+  
+      //ici on récupère des données de local storage (variables nommées plus haut)
+      let productColor = document.createElement("p")
+      productDiv3.appendChild(productColor)
+      productColor.innerHTML = colorProduct
+  
+      let productPrice = document.createElement("p")
+      productDiv3.appendChild(productPrice)
+      productPrice.innerHTML = productDescription.price * quantityProduct + ',00 €'
+  
+  
+      let productDiv4 = document.createElement("div")
+      productDiv2.appendChild(productDiv4)
+      productDiv4.classList.add("cart__item__content__settings")
+  
+      let productDiv5 = document.createElement("div")
+      productDiv4.appendChild(productDiv5)
+      productDiv5.classList.add("cart__item__content__settings__quantity")
+  
+  
+  
+      //ici on récupère des données de local storage (variables nommées plus haut)
+      let productQuantity = document.createElement("p");
+      productDiv5.appendChild(productQuantity);
+      productQuantity.innerHTML = 'Qté :' ;
+
+      let productInput = document.createElement("input");
+      productDiv5.appendChild(productInput);
+      productInput.setAttribute("type","number");
+      productInput.setAttribute("name","itemQuantity");
+      productInput.setAttribute("min","1");
+      productInput.setAttribute("max","100");
+      productInput.setAttribute("value", quantityProduct);
+      productInput.classList.add("itemQuantity");
+      
+
+      let productDiv6 = document.createElement("div");
+      productDiv4.appendChild(productDiv6);
+      productDiv6.classList.add("cart__item__content__settings__delete");
+
+      let productDelete = document.createElement("p");
+      productDiv6.appendChild(productDelete);
+      productDelete.classList.add("deleteItem");
+      productDelete.innerHTML ='Supprimer';
+
+//supprimer un produit du panier : selectionner le bouton"supprimer"
+let btnRemoveList= document.querySelectorAll(".deleteItem");
+      console.log(btnRemoveList)
+//déclarer la fonction removeFromCart..
+ function removeFromCart(product){
+ for(let product of inCart){
+ inCart = inCart.filter(element => element._id != product._id);  
+ console.log(inCart)
+ }}
+
+ //et s'en servir ensuite:
+ for (let btnRemove of btnRemoveList) {
+  btnRemove.addEventListener ("click",(e) =>{
+  e.preventDefault();  
+  removeFromCart(btnRemove.closest('.cart__item'));  
+   })}
+
+//changer la qté du produit sur la page panier
+
+//qté affichée à récuperer / puis à  enregistrer sur local Storagepuis à reafficher (getItem) sur la page
+
+//déclarer variable de la quantité modifiée
+
+ let itemQuantities = document.querySelectorAll('.itemQuantity');
+ console.log(itemQuantities);
+//déclarer la fonction modifyQuantity
+function modifyQuantity(){
+  for(let itemQuantity of itemQuantities){  
+    itemQuantity.addEventListener("change", (e)=>{
+    e.preventDefault();  
+  localStorage.setItem ('modifiedQuantity', itemQuantity.value)
+  localStorage.getItem ('modifiedQuantity', itemQuantity.value)
+  })
+  }
+  }
+
+  modifyQuantity()
 
 
-const getProducts= async()=>{    
-    await fetch('http://localhost:3000/api/products/')    
 
-    .then((response)=>  response.json())
-    .then ((promise) => 
-      products = promise);
-      console.log(products); 
-    }
-
-
- // ...pour n'afficher que les produits dont l'ID est mis en panier    
- async function identifyProduct()  {  
-        await getProducts() ;
-    
-
- let productIntoCart=products.find(element=>element._id===idProduct);
- console.log(productIntoCart);
-
-//puis afficher ci dessous les élements du produit du panier:
-let productArticle=document.createElement("article")
-document.getElementById('cart__items').appendChild(productArticle)
-productArticle.classList.add("cart__item")
-
-let productDiv1=document.createElement("div")
-productArticle.appendChild(productDiv1)
-productDiv1.classList.add("cart__item__img")
-
-let imgProduct=document.createElement("img")
-productDiv1.appendChild(imgProduct)
-imgProduct.setAttribute("src", productIntoCart.imageUrl);   
-imgProduct.setAttribute("alt", productIntoCart.altTxt);
-
-let productDiv2=document.createElement("div")
-productArticle.appendChild(productDiv2)
-productDiv2.classList.add("cart__item__content")
-
-let productDiv3=document.createElement("div")
-productDiv2.appendChild(productDiv3)
-productDiv3.classList.add("cart__item__content__description")
-
-let productName=document.createElement("h2")
-productDiv3.appendChild(productName)
-productName.innerHTML=productIntoCart.name
-
-//ici on récupère des données de local storage (variables nommées plus haut)
-let productColor=document.createElement("p")
-productDiv3.appendChild(productColor)
-productColor.innerHTML= colorProduct
-
-let productPrice=document.createElement("p")
-productDiv3.appendChild(productPrice)
-productPrice.innerHTML= productIntoCart.price +',00 €'
-
-
-let productDiv4=document.createElement("div")
-productDiv2.appendChild(productDiv4)
-productDiv4.classList.add("cart__item__content__settings")
-
-let productDiv5=document.createElement("div")
-productDiv4.appendChild(productDiv5)
-productDiv5.classList.add("cart__item__content__settings__quantity")
-
-
-
-//ici on récupère des données de local storage (variables nommées plus haut)
-let productQuantity=document.createElement("p")
-productDiv5.appendChild(productQuantity)
-productQuantity.innerHTML= 'Qté : ' + quantityProduct
-
+    });
 }
 
-//question : si letProductIntoCart est defini à l'intérieur de la fonction, je ne pourrais donc pas le reutiliser en dehors ??
 
 
 
-getProducts()
-identifyProduct()
+ 
 
 
-//afficher les donnée avec productintoCart(faire d'abord createElement puis appendChild)
-//document.querySelector(".cart_items_content_description>p").innerHTML=localStorage.getItem("colors");
-//document.querySelector(".cart_items_content_settings_quantity>p").innerHTML="Qté +" + localStorage.getItem("quantity");
+//changer la qté du produit sur la page panier
+
+//qté affichée à récuperer / puis à  enregistrer sur local Storagepuis à reafficher (getItem) sur la page
+
+//déclarer variable de la quantité modifiée
+
+// let itemQuantities = document.querySelectorAll('itemQuantity');
+
+// function modifyQuantity(){
+//   for(let itemQuantity of itemQuantities){
+
+//     itemQuantity.addEventListener(change, (e)=>{
+//     e.preventDefault();
+
+//  localStorage.setItem ('modifiedQuantity', itemQuantity.value)
+//  localStorage.getItem ('modifiedQuantity', itemQuantity.value)
+// })
+// }
+// }
+
+
+
+
+
--- front/js/product.js
+++ front/js/product.js
@@ -1,105 +1,113 @@
-//retrouver l'url de la page active :
-//let str =window.location.href;
 
-
-
-//récupérer l'id dans l'url de la page active
-//let MyUrl = new URL(str);
-//let idProduct = MyUrl.searchParams.get("idProduct");
-
-//c'est ok
-
-
-    //Récupération de l'id via les paramètres de l'url
-//const idProduct = new URL(window.location.href).searchParams.get("idProduct")
-
-
-const idProduct= window.location.search.split('?id=').join("")
-console.log (idProduct)
-
-
-
-//Récupération des sélecteurs pour les futures modifications
- //let titleProduct = document.getElementById("title");
- //let priceProduct = document.getElementById("price");
-//let descriptionProduct = document.getElementById("description");
- //let colorsProduct = document.getElementById("colors");
-// let imgProduct = document.querySelector(".item__img");
-
-
-
-
+const idProduct = new URL(window.location.href).searchParams.get("id");
 
 //aller chercher l'article correspondant à l'id de la page :
 
-        // definir variable product 
+// var url_string = window.location.href;
+// var url = new URL(url_string);
+// var idProduct = url.searchParams.get("c");
 
-let product=[]
+console.log("idProduct:" + idProduct);
 
-        //aller chercher dans l'API LE product
+let product = {};
 
-const getProduct= async()=>{    
-      await fetch('http://localhost:3000/api/products/'+idProduct)    
-
-    .then((response)=>  response.json())
-    .then ((promise) => 
-      product = promise);
-      console.log(product);
-       
+const getProduct = async () => {
+  await fetch('http://localhost:3000/api/products/' + idProduct)
+    .then((response) => response.json())
+    .then((promise) => product = promise);
+  console.log(product);
 }
-
-
-getProduct() 
-
-
 
 //afficher le produit
 
-async function displayProduct()  {  
- await getProduct() ;
+async function displayProduct() {
+  await getProduct();
 
-   document.getElementById("title").innerHTML=product.name;
-    document.getElementById("description").innerHTML=product.description;
-    document.getElementById("price").innerHTML=product.price;  
-   
-       
-   
-   let imgProduct=document.querySelector(".item__img>img")
-    imgProduct.setAttribute("src", product.imageUrl);   
-    imgProduct.setAttribute("alt", product.altTxt);
+  document.getElementById("title").innerHTML = product.name;
+  document.getElementById("description").innerHTML = product.description;
+  document.getElementById("price").innerHTML = product.price;
 
-    
+  let imgProduct = document.querySelector(".item__img>img")
+  imgProduct.setAttribute("src", product.imageUrl);
+  imgProduct.setAttribute("alt", product.altTxt);
 
-    for (let i=0; i < product.colors.length; i++) {
-      let color = document.createElement("option");
-      color.setAttribute("value", product.colors[i]);
-      color.innerHTML = product.colors[i];
-      document.getElementById("colors").appendChild(color);
+  for (let i = 0; i < product.colors.length; i++) {
+    let color = document.createElement("option");
+    color.setAttribute("value", product.colors[i]);
+    color.innerHTML = product.colors[i];
+    document.getElementById("colors").appendChild(color);
+  }
 
-    }
+  addIntoCart(product);
 }
 
-displayProduct()
- 
+displayProduct();
 
-//recuperer les données du produit choisi, et les stocker dans localStorage
-let boutonAjouter = document.querySelector("#addToCart");
 
-boutonAjouter.addEventListener("click", ()=>{
 
-localStorage.setItem ("_id", idProduct);
-localStorage.setItem ("colors", color);
-localStorage.setItem ("quantity", document.querySelector("#quantity").value);
 
-console.log(localStorage.quantity)
 
-})
+//definir la fonction Ajouter au panier:
+let addIntoCart = () => {
+  console.log('bonjour')
 
-        
+  //declarer le bouton ajouter
+  let boutonAjouter = document.querySelector("#addToCart");
 
-    
-            
-        
-/*.catch (function(error){
-    alert(error)   */
-    
+  // creer un evenement au clic
+  boutonAjouter.addEventListener("click", (event) => {
+    event.preventDefault();
+    //=toute action par défaut, normalement exécutée par le navigateur, n'aura pas lieu.
+
+
+    //recuperer les données du produit choisi (pour plus tard les stocker dans localStorage)
+    let colorProduct = document.getElementById("colors").value;
+    console.log('colorProduct :' + colorProduct)
+    let quantityProduct = document.getElementById("quantity").value;
+    console.log('quantityProduct :' + quantityProduct)
+
+    //creer l'objet qui contient les infos qu'on mettra dans localStorage
+    let selectedProduct = {
+      _id: idProduct,
+      colors: colorProduct,
+      quantity: quantityProduct
+    };
+
+    //console.log('selectedProduct :' + selectedProduct)
+
+    //définir la variable qui recupere dans localStorage la valeur (selectedProduct) liée à la à la clé "cart" 
+    let inCart = JSON.parse(localStorage.getItem('cart'));
+    console.log("inCart:" + inCart);
+
+    //si le panier ne contient pas d' article (il faut alors creer tableau inCart, et clé 'cart')
+    if (inCart == null) {
+      inCart = [];
+      inCart.push(selectedProduct);
+      localStorage.setItem('cart', JSON.stringify(inCart));
+    }
+    //si le panier n'est pas vide :
+    else {
+      for (let object of inCart) {
+
+        console.log('selectedProduct._id : ' + selectedProduct._id)
+        console.log('object.quantity : ' + object.quantity)
+        console.log('selectedProduct.quantity : ' + selectedProduct.quantity)
+
+        //si l'on ajoute un autre produit de même id et de même couleur, la quantité doit être la somme des 2 quantités
+        if (object._id == selectedProduct._id && object.colors == selectedProduct.colors) {
+          object.quantity = parseInt(selectedProduct.quantity) + parseInt(object.quantity);
+          localStorage.setItem('cart', JSON.stringify(inCart));
+          return;
+        }
+      }
+
+      // le panier n'est pas vide, et ne contient pas dejà le produit qu'on ajoute
+      inCart.push(selectedProduct);
+      console.log(selectedProduct);
+      localStorage.setItem('cart', JSON.stringify(inCart));
+    }
+  })
+};
+
+
+
